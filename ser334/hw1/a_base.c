b098189f5232e9de49819a9f905436bd16791c9d
Jaime Rabago Jr <jrabago@asu.edu>
2018-01-22 14:46:21 -0700

    hw part 1 done

--- ser334/hw1/a_base.c
+++ ser334/hw1/a_base.c
@@ -1,10 +1,11 @@
 /**
-* (basic description of the program or class)
+* compute the volume for a # of cylinders and total volume
 *
 * Completion time: (estimation of hours spent on this program)
 *
-* @author (your name), (anyone else, e.g., Acuna, whose code you used)
-* @version (a version number or a date)
+* @author JJ Rabago Jr 
+* @version 1.1.22
+*
 */
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -13,20 +14,31 @@
 
 int main(void)
 {
-	int multi_array[2][6] = {{10000, 2, 3, 9, 10, 10}, {7, 8, 9, 9, 1999, 0}};
-	int size = sizeof(multi_array);
-	printf("%d",size);
+	int cNum, i=0;
+ 	float hei, rad, vol, tVol=0;
 	//Add a prompt for the number of cylinders to sum. Make sure to use the control
 	//symbol for integers. [2 points]
 
+	printf("how many cylinders to sum? ");
+	scanf("%d", &cNum);
+	
 	//Create a loop based on the number of cylinders the user enters. [2 points]
-
-	//    Within the loop, prompt for height and radius parameters (allow floating
-	//    point numbers), and calculate the volume for that particular cylinder.
-	//    [4 points]
-
+	while(i<cNum){
+		//    Within the loop, prompt for height and radius parameters (allow floating
+		//    point numbers), and calculate the volume for that particular cylinder.
+		//    [4 points]
+	
+		printf("height of cylinder: \n");
+		scanf("%f", &hei);
+		printf("radius of cylinder: \n");
+		scanf("%f", &rad);
+		vol = 3.14159 * rad * rad * hei;
+		tVol += vol;
+		i++;
+	}
 	//Display the total volume sum back to the user. Make sure to use the right control
 	//symbol. [2 points]
+	printf("total volume of cylinder(s): %f\n", tVol);
 
 	return 0;
 }
