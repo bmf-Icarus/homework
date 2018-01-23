f57cc1645fa68cd721cf774d9c2869bfddeec8b7
Jaime Rabago Jr <jrabago@asu.edu>
2018-01-22 17:38:44 -0700

    running program in different env

--- ser334/hw1/b_base.c
+++ ser334/hw1/b_base.c
@@ -1,5 +1,5 @@
 /**
-* discription of program here
+* build algorithm from pseudocode given "Collatz"
 *
 * Completion time: (estimation of hours spent on this program)
 *
@@ -12,33 +12,35 @@
 //INCLUDES
 #include <stdio.h>
 
-int main(void)
-{
-	int cNum, i=0;
- 	float hei, rad, vol, tVol=0;
-	//Add a prompt for the number of cylinders to sum. Make sure to use the control
-	//symbol for integers. [2 points]
+int termination(int n);
 
-	printf("how many cylinders to sum? ");
-	scanf("%d", &cNum);
-	
-	//Create a loop based on the number of cylinders the user enters. [2 points]
-	while(i<cNum){
-		//    Within the loop, prompt for height and radius parameters (allow floating
-		//    point numbers), and calculate the volume for that particular cylinder.
-		//    [4 points]
-	
-		printf("height of cylinder: \n");
-		scanf("%f", &hei);
-		printf("radius of cylinder: \n");
-		scanf("%f", &rad);
-		vol = 3.14159 * rad * rad * hei;
-		tVol += vol;
-		i++;
-	}
-	//Display the total volume sum back to the user. Make sure to use the right control
-	//symbol. [2 points]
-	printf("total volume of cylinder(s): %f\n", tVol);
+int main(void){
+
+	int num, ans;
+
+	printf("Give me an integer please: ");
+	scanf("%d\n", &num);	
+	ans = termination(num);
+	printf("Given integer was terminated %d times!\n", ans);
 
 	return 0;
+}
+
+
+int termination(int n){
+	int i=0;
+	while(n != 1){
+		printf("while loop\n");
+		if(!(n % 2)){
+			printf("n is %d\n", n);
+			n = n/2;
+		}else{
+			printf("n is %d\n", n);
+			n = (3*n)+1;
+		}
+	
+		i++;
+
+	}
+	return i;
 }
