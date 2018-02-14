0db250a331c2aaaa4837837bd9cf46bcd06005e6
Jaime Rabago Jr <jrabago@asu.edu>
2018-02-13 21:18:21 -0700

    fuck it

--- ser334/hw3/RabagoLKM.c
+++ ser334/hw3/RabagoLKM.c
@@ -2,6 +2,7 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/sched.h>
+#include <linux/moduleparam.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("JJ Rabago Jr");
@@ -9,24 +10,28 @@ MODULE_DESCRIPTION("hw3");
 MODULE_VERSION("1.0");
 
 static int inp_pid=0;
+module_param(inp_pid, int, 0);
 
 static struct task_struct *p;
+static struct task_struct *q;
 static struct list_head *head;
 
 static int __init start(void){
 
-	printk("PROCESS NAME\tPID\tSTATE\tPRIORITY\tS_PRIORITY\tN_PRIORITY\n");
 	for_each_process(p){
-		printk(" %-11s\t%-4d\t%-4ld\t%-8d\t%-8d\t%-8d\n"
-		,p->comm,p->pid,p->state,p->prio,p->static_prio,p->normal_prio); 
-		
-		printk("CHILDREN\n");
-		
-		list_for_each( head , &p->children ){
-			 p = list_entry( head, struct task_struct, children );
+		if(inp_pid < p->pid){
+			printk("PROCESS NAME\tPID\tSTATE\tPRIORITY\tS_PRIORITY\tN_PRIORITY\n");
+			printk("PARENT\n");
 			printk(" %-11s\t%-4d\t%-4ld\t%-8d\t%-8d\t%-8d\n"
-		,p->comm,p->pid,p->state,p->prio,p->static_prio,p->normal_prio); 
+			,p->comm,p->pid,p->state,p->prio,p->static_prio,p->normal_prio); 
 			
+			printk("CHILDREN\n");
+			list_for_each( head , &p->children){
+				q = list_entry( head, struct task_struct, sibling );
+				printk(" %-11s\t%-4d\t%-4ld\t%-8d\t%-8d\t%-8d\n"
+				,q->comm,q->pid,q->state,q->prio,q->static_prio,q->normal_prio); 
+			}
+			printk("\n");
 		}
 	}
 
