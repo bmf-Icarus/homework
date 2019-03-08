15f08ef89f2e8d04d9ec55b38db4a97386162392
bmf-Icarus <jrabago@asu.edu>
2019-03-08 12:38:52 -0700

    updated file

--- ser486/p1/uart.h
+++ ser486/p1/uart.h
@@ -1,24 +1,25 @@
-/*
-    uart.h
-
-    this header file includes function declarations for the SER486
-    Homework assignment 2
+/********************************************************
+ * uart.h
+ *
+ * this file provides function declarations for SER486
+ * uart library functions.
+ *
+ * Author:   Doug Sandy
+ * Date:     2/16/2018
+ * Revision: 1.0
+ *
+ * Copyright(C) 2018, Arizona State University
+ * All rights reserved
+ *
  */
+#ifndef UART_H_INCLUDED
+#define UART_H_INCLUDED
 
-#ifndef _UART_H
-#define _UART_H
+void uart_init(void);                   /* initialize the uart hardware */
+void uart_writechar(char ch);           /* write a single character to the uart */
+void uart_writestr(char * str);         /* write an ascii string to the uart */
+void uart_writehex8(unsigned char num); /* output a 8-bit hexadecimal value as ascii text */
+void uart_writehex16(unsigned int num); /* output a 16-bit hexadecimal value as ascii text */
+void uart_writedec32(signed long num); /* output a 32-bit signed decimal value as ascii text */
 
-
-/* initialize uart subsystem - this function must be called once before
-   the serial port can be used.
-*/
-void uart_init(void);
-
-/*
-   This function will write a single character to the atmega uart.
-   uart_init() must be called once before this function will work.
-*/
-void uart_writechar(char ch);
-
-#endif  /* _UART_H */
-
+#endif // UART_H_INCLUDED
