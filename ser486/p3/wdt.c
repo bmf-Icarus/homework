174db32a5c16bccfd64be1963989bb1708f19f65
bmf-Icarus <jrabago@asu.edu>
2019-04-10 08:41:13 -0700

    started on wdt, debug fxs

--- ser486/p3/wdt.c
+++ ser486/p3/wdt.c
@@ -7,18 +7,41 @@
  *	possibly save logs or other vital data.
  */
 
- void wdt_init()
- {
+#define MCUSR (*(volatile char *)0x54)
+#define WDTCSR (*(volatile char *)0x60)
+#define SREG (*(volatile char *)0x5F)
 
- }
+void wdt_init()
+{
+    // store sreg state and disable interrupts
+    char temp = SREG;
+    SREG &= 0x7F;
+    // for changing wd registers bit 3 must be set first
+    WDTCSR = 0x14;
+    // bit 6 WDIE, WDE, and WDP(pre-scaler ~2s)
+    WDTCSR = 0x4F;
+    // ensure that SREG state is restored
+    SREG = temp;
+}
 
- void wdt_reset()
- {
+void wdt_reset()
+{
+    //assembly language reset watchdog timer
+    asm("WDR");
 
- }
+}
 
- void wdt_force_restart()
- {
+void wdt_force_restart()
+{
+    // set forge restart
+    __vector_6();
+}
 
- }
+void __vector_6(void) __attribute__ ((signal, used, externally_visible));
+void __vector_6(void)
+{
+    //send out messages to log for info logging
 
+
+    while(true){} // busy waiting loop
+}
