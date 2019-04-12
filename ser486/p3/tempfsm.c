49d829c79c6008e42a07031a8211af85daef38e2
bmf-icarus <jrabago@asu.edu>
2019-04-12 13:53:34 -0700

    tempfsm start

--- ser486/p3/tempfsm.c
+++ ser486/p3/tempfsm.c
@@ -7,9 +7,25 @@
  *	temps are detected to reduce emergency line chatter.
  */
 
+#include "config.h"
+
+
+#define NORM1      0
+#define NORM2      1
+#define NORM3      2
+#define WARNLO1    3
+#define WARNLO2    4
+#define CRITLO     5
+#define WARNHI1    6
+#define WARNHI2    7
+#define CRITHI     8
+
+static int t_state;
 
 void tempfsm_init()
 {
+    t_state = NORM1;
+
 
 }
 
@@ -20,5 +36,5 @@ void tempfsm_update(int current, int hicrit, int hiwarn, int locrit, int lowarn)
 
 void tempfsm_reset()
 {
-
+    t_state = NORM1;
 }
